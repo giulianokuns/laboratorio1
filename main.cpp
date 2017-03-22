178da5169644e7e6bdc7fab78c212c9d790f07c7
Jose Antonio Esteche Ferreira - ENS <jose.antonio.esteche@pcunix33.fing.edu.uy>
2017-03-21 21:28:17 -0300

    menu a medias

--- main.cpp
+++ main.cpp
@@ -70,99 +70,160 @@ using namespace std;
 		}
 	}
 
-	void agregarInscripcion(String ciSocio, int idClase, Fecha fecha) {
 
-	}
-	void borrarInscripcion(string ciSocio, int idClase) {
+        void agregarInscripcion(string ciSocio, int idClase, Fecha fecha){
+                int i=0;
+                //se busca la clase donde se debe inscribir al socio
+                while(i<tope_clase)&&(clases[i].getid()!=idClase)
+                i++;
+                if (i>tope_clases)
+                throw std::invalid_argument("No existe la clase");
+                else if (clases[i].cupo==0)
+                throw std::invalid_argument("No hay cupos");
+                else {
+                int cantidad=clases[i].getCantInscriptos;
+                Inscripcion **inscriptos=clases[i].getInscriptos();
+                /*if (dynamic_cast<Entrenamiento &> c[i] !=NULL)
+                    if (c[i].getenRambla())
+                        capacidad=20;
+                    else
+                        capacidad=10;
+                else
+                    capacidad=50;*/
 
-	}
-
-	DtSocio** obtenerInfoSociosPorClase (id idClase, int& cantSocios) {
-
-	}
-
-void agregarInscripcion(string ciSocio, int idClase, Fecha fecha){
-	int i=0;
-	//se busca la clase donde se debe inscribir al socio
-	while(i<tope_clase)&&(clases[i].getid()!=idClase)
-	   i++;
-	if (i>tope_clases)
-	  throw std::invalid_argument("No existe la clase");
-	else if (clases[i].cupo==0)
-	   throw std::invalid_argument("No hay cupos");
-	else {
-	   int cantidad=clases[i].getCantInscriptos;
-	   Inscripcion **inscriptos=clases[i].getInscriptos();
-	   /*if (dynamic_cast<Entrenamiento &> c[i] !=NULL)
-	      if (c[i].getenRambla())
-	         capacidad=20;
-	      else
-	         capacidad=10;
-	   else
-	      capacidad=50;*/
-
-	   //itero enel arreglo de inscriptos buscando la inscripción
-	   int j=0;
-	   bool esta=false;
-	   while (j<cantidad)&&(!(esta)){
-	       esta=(strcomp(((**inscriptos[j]).getSocio()).getCI(),ciSocio)==0);// en caso de no funcionar, probar inscriptos[i]->getSocio()->getCI() para el primer argumento
-	       j++;
-	   }
-	   if (j<cantidad)
-	       throw std::invalid_argument("Ya está inscripto");
-	   else{
-           agregarSocio(ciSocio,clases[i].getNombre());
-           Inscripcion(*socio[tope_socios],*fecha);
-	   }
+                //itero enel arreglo de inscriptos buscando la inscripción
+                int j=0;
+                bool esta=false;
+                while (j<cantidad)&&(!(esta)){
+                    esta=(strcomp(((**inscriptos[j]).getSocio()).getCI(),ciSocio)==0);// en caso de no funcionar, probar inscriptos[i]->getSocio()->getCI() para el primer argumento
+                    j++;
+                }
+                if (j<cantidad)
+                    throw std::invalid_argument("Ya está inscripto");
+                else{
+                agregarSocio(ciSocio,clases[i].getNombre());
+                Inscripcion(*socio[tope_socios],*fecha);
+                }
 
 
-	}
+                }
 
-}
-void borrarInscriptos(string ciSocio, int idClase){
-    int i=0;
-    while (i<tope_clases)&&(clases[i].getid()!=idClase)
-    	i++;
-    if (i>tope_clases)
-    	throw std::invalid_argument("La clase no existe");
-    else{
-    	//busco la inscripción
-    	Inscripcion **inscriptos=c[i].getInscriptos();
-    	int cantidad=clases[i].getCantInscriptos();
-    	int j=0;
-    	bool esta= (strcmp(((**inscriptos[j]).getSocio()).getCI(),ciSocio)==0);
-    	while ((j<cantidad)&&(!(esta))){
-    		esta= (strcmp(((**inscriptos[j]).getSocio()).getCI(),ciSocio)==0);
-    		j++;
-    	}
-    	if (j>cantidad)
-    		throw std::invalid_argument("No hay inscriptos")
-    	else{
-    		inscriptos[j]=inscriptos[cantidad-1];
-    		clases[i].setInscriptos(cantidad-1);
-    	}
+        }
+        
+        void borrarInscripcion(string ciSocio, int idClase){
+            int i=0;
+            while (i<tope_clases)&&(clases[i].getid()!=idClase)
+                i++;
+            if (i>tope_clases)
+                throw std::invalid_argument("La clase no existe");
+            else{
+                //busco la inscripción
+                Inscripcion **inscriptos=c[i].getInscriptos();
+                int cantidad=clases[i].getCantInscriptos();
+                int j=0;
+                bool esta= (strcmp(((**inscriptos[j]).getSocio()).getCI(),ciSocio)==0);
+                while ((j<cantidad)&&(!(esta))){
+                        esta= (strcmp(((**inscriptos[j]).getSocio()).getCI(),ciSocio)==0);
+                        j++;
+                }
+                if (j>cantidad)
+                        throw std::invalid_argument("No hay inscriptos")
+                else{
+                        inscriptos[j]=inscriptos[cantidad-1];
+                        clases[i].setInscriptos(cantidad-1);
+                }
 
-    }
-}
-DtSocio** obtenerInfoSocioPorClase(int idClase,int& cantSocios){
-	int i=0;
-	while (i<tope_clases)&&(c[i].getid()!=idClase)
-		i++;
-	DtSocio *infoSocio[cantSocios];
-	if (i<tope_clases){
-	    Inscripcion **inscriptos=c[i].getInscriptos();
-	    for(int j=0; j<cantSocios;j++)
-	    	infoSocio[j]=inscriptos[j].getSocio();
+            }
+        }
+        
+        DtSocio** obtenerInfoSocioPorClase(int idClase,int& cantSocios){
+                int i=0;
+                while (i<tope_clases)&&(c[i].getid()!=idClase)
+                        i++;
+                DtSocio *infoSocio[cantSocios];
+                if (i<tope_clases){
+                    Inscripcion **inscriptos=c[i].getInscriptos();
+                    for(int j=0; j<cantSocios;j++)
+                        infoSocio[j]=inscriptos[j].getSocio();
 
-	}
-	else
-		infoSocio=NULL;
-	return infoSocio;
-}
+                }
+                else
+                        infoSocio=NULL;
+                return infoSocio;
+        }
 
 int main()
 {
     int opcion = 0;
-    while()
+    
+    while(opcion != 7){
+        
+        cout << "1. Agregar Socio" << endl;
+        cout << "2. Agregar Clase" << endl;
+        cout << "3. Agregar Inscripcion" << endl;
+        cout << "4. Borrar Inscripcion" << endl;
+        cout << "5. Imprimir Socios por clase" << endl;
+        cout << "6. Imprimir Clase" << endl << endl;
+        
+        cout << "Ingrese opcion:"
+        cin >> opcion;
+        cout << endl << endl;
+        
+        try{
+            if(opcion == 1){
+                
+                string ci,nombre;
+                
+                cout << "Ingrese ci:";
+                cin >> ci;
+                cout << endl;
+                
+                
+                cout << "Ingrese Nombre:";
+                cin >> nombre;
+                cout << endl;
+                
+                agregarSocio(ci,nombre);
+                
+            }
+            else if(opcion == 3){
+                
+            }
+            else if(opcion == 4){
+                
+            }
+            else if(opcion == 5){
+                
+                string ciSocio; int idClase;
+                
+                cout << "Ingrese ci:";
+                cin >> ciSocio;
+                cout << endl;
+                
+                
+                cout << "Ingrese Clase:";
+                cin >> idClase;
+                cout << endl;
+                
+                borrarInscripcion(ciSocio,idClase);
+                
+            }
+            else if(opcion == 6){
+                
+            }
+            else if(opcion == 7){
+                
+            }
+            else {
+                
+                invalid_argument("opcion no valida");
+            }
+        }catch(invalid_argument &ia){
+            
+            cout << "Error: " << ia.what() << endl;
+            
+        }
+    }
+    
     return 0
 }
