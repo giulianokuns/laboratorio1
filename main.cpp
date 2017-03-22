3135cecfacafa7c6b7f4c98120f83468a647c1eb
ruben2773 <rubensil1@hotmail.com>
2017-03-22 00:49:26 -0300

    ultima funciónterminada

--- main.cpp
+++ main.cpp
@@ -151,6 +151,19 @@ using namespace std;
                         infoSocio=NULL;
                 return infoSocio;
         }
+        DtClase obtenerClase(int idClase){
+	     //se asume que debe existir la clase del id correspondiente
+	            DtClase datosClase;
+	            int j=0;
+	            while(j<=tope_clases)&&(clases[j].getid()!=idClase)
+		            j++;
+	            if (j<tope_clases) //control de error
+		            if (dynamic_cast<Entrenamiento &> clases[j]!=NULL) 
+	                    datosClase.DtSpinning(clases[j].getCantBicicletas(),clases[j].getid(),clases[j].getNombre(),clases[j].getTurno());
+	            else
+	                datosClase.DtEntrenamiento(clases[j].getenRambla(),clases[j].getid(),clases[j].getNombre(),clases[j].getTurno());
+                return datosClase;
+        } 
 
 int main()
 {
