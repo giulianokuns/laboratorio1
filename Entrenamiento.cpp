001682210fd8e30157678b668caac97fee112c06
gk <giuliano.kuns@woow.com.uy>
2017-03-26 16:24:03 -0300

    #UPDATE TO COMPILE

--- Entrenamiento.cpp
+++ Entrenamiento.cpp
@@ -12,25 +12,25 @@ Entrenamiento::Entrenamiento(bool enRambla, int id, string nombre,Turno turno){
 	this->setid(id);
 	this->setNombre(nombre);
 	this->setTurno(turno);
-    Inscripcion **Inscripciones = new *Inscripcion[50];
+    Inscripcion **Inscripciones = new Inscripcion*[50];
 	this->setInscripciones(Inscripciones);
 	this->setInscriptos(0);
 };
 
-int  Entrenamiento::getenRambla(){
+bool Entrenamiento::getenRambla(){
 	return enRambla;
-}
+};
 
-void  Entrenamiento::setenRambla(bool enRambla){
+void Entrenamiento::setenRambla(bool enRambla){
 	this->enRambla = enRambla;
-}
+};
 
 int Entrenamiento::cupo(){
 	if(enRambla)
 		20 - this->getCantInscriptos();
 	else
 		10 - this->getCantInscriptos();
-}
+};
 
 Entrenamiento::~Entrenamiento(){
 	Inscripcion **Inscripciones = this->getInscriptos();
@@ -38,4 +38,4 @@ Entrenamiento::~Entrenamiento(){
 		delete Inscripciones[i];
 	}
 	delete Inscripciones;
-}
+};
