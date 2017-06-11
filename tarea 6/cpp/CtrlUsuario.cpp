27c4af5f92ef346d9b410b0821a245ad4dc20259
Giuliano <giuliano.kuns@woow.com.uy>
2017-06-11 17:42:08 -0300

    #UPDATE: Creando ctrusuario para caso de uso ver Mensaje

--- tarea 6/cpp/CtrlUsuario.cpp
+++ tarea 6/cpp/CtrlUsuario.cpp
@@ -1,1 +1,13 @@
 //instancia = NULL
+
+#include "Fecha.h"
+#include "Hora.h"
+#include "Usuario.h"
+#include "Mensaje.h"
+
+/* OJO que no tengo idea si esta bien lo que estoy haciendo !!!*/
+class CtrlUsuario {
+	public:
+		DtMensajae * mensajesCoversacion (int idConv);
+
+};
