32fd9dd69bfa675ba8c45044ffbbcaff4fe2ecfe
ruben2773 <rubensil1@hotmail.com>
2017-06-15 15:46:13 -0300

    ctrlCasoEnviar.cpp y .h

--- tarea 6/cpp/Controllers/CtrlCasoEnviar.cpp
+++ tarea 6/cpp/Controllers/CtrlCasoEnviar.cpp
@@ -2,14 +2,28 @@
 #include "Mensaje.h"
 #include "Usuario.h"
 
-int cantidadArchivadas(){
-	return (this->getCtrlUsuario()).cantidadArchivadas();
+// Singleton
+CtrlCasoEnviar * CtrlCasoEnviar::getInstancia(){
+	if (instancia == NUL)
+		this->instancia = new CtrlCasoEnviar;
+	return instancia;
 }
 
-ListaDtConversacion * listarArchivadas(){
-	return (this->getCtrlUsuario()).listarArchivadas();
+int CtrlCasoEnviar::cantidadArchivadas(){
+	CtrlUsuario:: *ctrl = getInstancia();
+	return this->ctrl.cantidadArchivadas();
+}
+
+ICollection * CtrlCasoEnviar::listarArchivadas(){
+	CtrlUsuario:: *ctrl = getInstancia(),
+	return this->ctrl.listarArchivadas();
 }
 	   
-ListaDtConversacion * listarActivas(){
-	return (this->getCtrlUsuario()).listarActivas();
+ICollection * CtrlCasoEnviar::listarActivas(){
+	CtrlUsuario:: *ctrl = getInstancia();
+	return this->ctrl.listarActivas();
 }
+
+ string * CtrlCasoEnviar::crearConvrsacion(string idContact){
+   
+ }
