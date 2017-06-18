0caa7b87d9301bdbdeaef0e993ffa757eb30e488
Jose Antonio Esteche Ferreira - ENS <jose.antonio.esteche@pcunix107.fing.edu.uy>
2017-06-18 19:32:56 -0300

    main

--- tarea6/main.cpp
+++ tarea6/main.cpp
@@ -79,6 +79,18 @@ using namespace std;
             
             }
             
+            if(!seDioDeAlta){
+                /*MOSTRAR NOTIFICACIONES*/
+                cout << endl << endl << endl
+                ICollection notificaciones = instancia->getNotificaciones();
+                for(IIterator *it = notificaciones->getIterator(); it->hasCurrent(); it-next()){
+                    DtNotificaciones notificacion = dynamic_cast<DtNotificaciones *>(it->getCurrent());
+                    cout << "El usuario :" << notificacion->gettelCel() << "Cambio su " << notificacion->getTipo() << "Por " << notificacion->getDesc() << endl;
+                    cout << notificacion->getFecha()->getDia() << "/" << notificacion->getFecha()->getMes() << "/" << notificacion->getFecha()->getAño() << " " << notificacion->getHora()->getHora() << ":" << notificacion->getHora()->getMinutos();
+                    cout << endl << endl;
+                }
+                instancia->eliminarNotificaciones();
+            }
         }
 
 /*CASO DE USO: AGREGAR CONTACTO*/
@@ -881,3 +893,207 @@ int main()
     }
     return 0;
 }
+
+/*CASO DE USO: ELIMINAR MENSAJE*/
+
+void eliminarMensaje(){
+    
+    ICtrlUsuario *CI = ICtrlUsuario::getinstancia();
+
+Usuario * user_log = CI->getusuarioLog();
+
+if (user_log != NULL) {
+	ICollection * lista_activas = CI->listarActivas();
+	int cantidad_archivadas     = CI->cantidadArchivadas();
+
+	if (!lista_activas.isEmpty()) {
+		string nombre, tel_cel;
+		cout << "Conversaciones activas" << endl;
+
+		for (IIterator *it = lista_activas->getIterator(); it->hasCurrent(); it->next()) {
+			Conversacion * c = getCurrent();
+			bool es_grupo = c->getesGrupo();
+			if (es_grupo) {
+				Grupo g = c->getgrupo();
+				nombre = g->getnomGrupo();
+			} else {
+				IDictionary * participantes = getparticipantes();
+				/*Es una conversacion Simple, tiene 2 participantes, si no es el usuario logeado entonces es el
+				otro*/
+				for (IIterator *it_p = participantes->getIterator(); it_p->hasCurrent(); it_p->next()) {
+					Usuario * u = getCurrent();
+					if (!userlog->gettelCel()->compare(u->gettelCel())) {
+						nombre  = u->getnomUsuario();
+						tel_cel = u->gettelCel();
+					}
+				}		
+			}
+			cout << nombre + " " + tel_cel << endl;
+		}
+		cout << "" << endl;
+		cout << "Archivadas :" + cantidad_archivadas << endl;
+
+		cout << "****************************************" 	<< endl;
+		cout << "1. Seleccionar una conversación activa " 	<< endl;
+		cout << "2. Ver las conversaciones archivadas" 		<< endl;
+		int opcion;
+		cout << "Opción: ";
+		cin >> opcion;
+
+		if (opcion == 1) {
+			cout << "Ingrese el identificador de la conversación activa que desea seleccionar: ";
+			int idC;
+			cin >> idC;
+			IKey * idConv = new int (idC);
+			ICollection * mensajes =  CI->mensajesCoversacion (idConv);
+
+			if (!mensajes->isEmpty()) {
+				for (IIterator *it_m = mensajes->getIterator(); it_m->hasCurrent(); it_m->next()) {
+					Mensaje m = getCurrent();
+
+					if (dynamic_cast<Simple*> (&m) != NULL) {
+						//Es Simple
+						Simple * mensj = dynamic_cast<Simple*> (&m);
+						cout << * mensj << endl;
+					} else if (dynamic_cast<Contacto*> (&m) != NULL) {
+						//Es Contacto
+						Contacto * mensj = dynamic_cast<Contacto*> (&m);
+						cout << * mensj << endl;
+					} else {
+						//Es Multimedia
+						if (dynamic_cast<Imagen*> (&m) != NULL) {
+							//Es Imagen
+							Imagen * mensj = dynamic_cast<Imagen*> (&m);
+							cout << * mensj << endl;
+						} else {
+							//Es Video
+							Video * mensj = dynamic_cast<Video*> (&m);
+							cout << * mensj << endl;
+						}
+					}
+				}
+						
+				cout << "Ingrese el código del mensaje que desea eliminar.";
+				string codigo;
+				cin >> codigo;
+
+				IKey * codigo = new String (codigo);
+				CI->eliminarMensaje(codigo, idConv);
+
+			} else {
+				throw std::invalid_argument("No hay mensajes para mostrar.");
+			}				
+		} else if (opcion == 2) {
+			cout << "Conversaciones archivadas"	<< endl;
+			ICollection * lista_archivadas = CI->listarArchivadas();
+
+			if (!lista_archivadas->isEmpty()) {
+				for (IIterator *it = lista_archivadas->getIterator(); it->hasCurrent(); it->next()) {
+					Conversacion * c = getCurrent();
+					bool es_grupo = c->getesGrupo();
+					if (es_grupo) {
+						Grupo g = c->getgrupo();
+						nombre = g->getnomGrupo();
+					} else {
+						IDictionary * participantes = getparticipantes();
+						/*Es una conversacion Simple, tiene 2 participantes, si no es el usuario logeado entonces es el
+						otro*/
+						for (IIterator *it_p = participantes->getIterator(); it_p->hasCurrent(); it_p->next()) {
+							Usuario * u = getCurrent();
+							if (!userlog->gettelCel()->compare(u->gettelCel())) {
+								nombre  = u->getnomUsuario();
+								tel_cel = u->gettelCel();
+							}
+						}		
+					}
+					cout << nombre + " " + tel_cel << endl;	
+				}
+				cout << "Ingrese el identificador de la conversación archivada que desea seleccionar: ";
+				int idC;
+				cin >> idC;
+				IKey * idConv = new int (idC);
+
+				ICollection * mensajes =  CI->mensajesCoversacion (idConv);
+				
+				if (!mensajes->isEmpty()) {
+					for (IIterator *it_m = mensajes->getIterator(); it_m->hasCurrent(); it_m->next()) {
+						Mensaje m = getCurrent();
+
+						if (dynamic_cast<Simple*> (&m) != NULL) {
+							//Es Simple
+							Simple * mensj = dynamic_cast<Simple*> (&m);
+							cout << * mensj << endl;
+						} else if (dynamic_cast<Contacto*> (&m) != NULL) {
+							//Es Contacto
+							Contacto * mensj = dynamic_cast<Contacto*> (&m);
+							cout << * mensj << endl;
+						} else {
+							//Es Multimedia
+							if (dynamic_cast<Imagen*> (&m) != NULL) {
+								//Es Imagen
+								Imagen * mensj = dynamic_cast<Imagen*> (&m);
+								cout << * mensj << endl;
+							} else {
+								//Es Video
+								Video * mensj = dynamic_cast<Video*> (&m);
+								cout << * mensj << endl;
+							}
+						}
+
+						cout << "Ingrese el código del mensaje que desea eliminar.";
+						string codigo;
+						cin >> codigo;
+
+						IKey * codigo = new String (codigo);
+						CI->eliminarMensaje(codigo, idConv);
+					}
+				} else {
+					throw std::invalid_argument("No hay mensajes para mostrar.");
+				}
+			} else {
+				throw std::invalid_argument("No tiene conversaciones archivadas.");
+			}
+		} else {
+			throw std::invalid_argument('Opción invalida.');
+		}
+	} else {
+		throw std::invalid_argument('No tiene conversaciones activas.');
+	}
+} else {
+	throw std::invalid_argument('No hay un usuario logeado en el sistema.');
+}
+    
+}
+
+/*CASO DE USO: AGREGAR SUSCRIPTORES*/
+
+void agregarSuscriptores(){
+    
+    ICtrlUsuario *CI = ICtrlUsuario::getinstancia();
+    string numero;
+    
+    cout << "Ingrese usuario al que desea suscribirse: ";
+    cin >> numero;
+    
+    CI->agregarSuscriptor(new String(numero));
+    
+}
+
+int main(){
+    
+    int op = 0;
+    
+    while(op != ?){
+        
+        cout << "1.Establecer fecha y hora del sistema" << endl;
+        cout << "2.Mostrar fecha y hora del sistema" << endl;
+        cout << "3.Abrir Guasap" << endl;
+        cout << "4.Cerar Guasap" << endl;
+        cout << "5.Agregar Contacto" << endl;
+        cout << "6.Alta Grupo" << endl;
+        cout << "7.Enviar Mensaje" << endl;
+        cout << "8.Ver Mensajes" << endl;
+        
+    }
+}
+
