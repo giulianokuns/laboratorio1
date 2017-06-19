4c9206f5b4e2c270e7d56698effe0498158206c9
rodrigo <rodrigonegreyra@github.com>
2017-06-19 19:08:31 -0300

    DtInfoIngreso compila

--- tarea6/cpp/DataTypes/DtInfoIngreso.cpp
+++ tarea6/cpp/DataTypes/DtInfoIngreso.cpp
@@ -3,9 +3,7 @@
 
 using namespace std;
 
-DtInfoIngreso::DtInfoIngreso(Fecha *fecha_ingreso, Hora *hora_ingreso){
-	this->dtFechaIngreso = new Fecha (fecha_ingreso->getdia(), fecha_ingreso->getmes(), fecha_ingreso->getanio());
-	this->dtHoraIngreso  = new Hora (hora_ingreso->gethoras(), hora_ingreso->getminutos());
+DtInfoIngreso::DtInfoIngreso(Fecha *fecha_ingreso, Hora *hora_ingreso) : dtFechaIngreso(fecha_ingreso->getdia(), fecha_ingreso->getmes(), fecha_ingreso->getanio()), dtHoraIngreso(hora_ingreso->gethoras(), hora_ingreso->getminutos()){
 }
 
 DtInfoIngreso::~DtInfoIngreso () {}
